f124bcc365e6a6cff4e43bade1815d17fb2f670b
NhaanThieen <nguyenvuthiennhan05@gmail.com>
2024-11-09 15:56:57 +0700

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-
 import './App.css';
-import io from 'socket.io-client';
+import io from 'socket.io-client'
+import { useEffect } from 'react'
 
 // Tạo đối tượng kết nối đến server 
 const socket = io.connect('http://localhost:5000');
@@ -10,10 +10,19 @@ function App() {
 
   const sendMessage = () => {
 
-
+    socket.emit("sendMessage", "hello");
 
   };
-  
+
+  // Chạy mỗi khi giá trị socket được gửi tới từ server
+  useEffect(() => {
+
+    socket.on("receiveMessage", (data) => {
+      alert(data);
+    })
+
+  }, []);
+
   return (
     <div className="App">
       <input placeholder='Input here' />
--- server/index.js
+++ server/index.js
@@ -6,11 +6,13 @@ const http = require("http");
 const { Server } = require('socket.io');
 // Import cors
 const cors = require('cors');
+const { Socket } = require('dgram');
+const { log } = require('console');
 var port = 5000;
-
 
 // Khai báo đối tượng của express
 const app = express()
+app.use(cors());
 
 // Khai báo server chạy bằng giao thức http dựa trên đối tượng của express
 const server = http.createServer(app);
@@ -18,15 +20,27 @@ const server = http.createServer(app);
 // Tạo đối tượng đại diện cho socket.io (tích hợp thư viện vào server)
 const io = new Server(server, {
     cors: {
-        // Địa chỉ của front-end
-        origin: `http://localhost:3000`,
+        // Địa chỉ tới front-end
+        origin: "http://localhost:3000",
         methods: ["GET", "POST"],
     },
 });
 
 
-io.on('connection', () => {
-    console.log('A user connected');
+// Lắng nghe sự kiện kết nối của client, biến socket đại diện cho client
+io.on('connection', (socket) => {
+    console.log("A user connected");
+
+    socket.on("sendMessage", (data) => {
+        // Gửi tới toàn bộ client khác đang kết nối trừ client gửi
+        socket.broadcast.emit("receiveMessage", data);
+        socket.to().emit
+    })
+
+    socket.on('disconnect', () =>{
+        console.log("A user disconnected");
+    })
+
 });
 
 
